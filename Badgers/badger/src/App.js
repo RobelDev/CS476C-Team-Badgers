bab0e67d0987d90ce77468871d5bd61775680618
Robel Tegegne <robelkTegegne@gmail.com>
2020-11-12 15:42:36 -0700

    Development is set up, Happy coding team.

---
+++ Badgers/badger/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Development starts, Happy coding team -R</h1>
+      <h2>this will be our single page application</h2>
+    </div>
+  );
+}
+
+export default App;
